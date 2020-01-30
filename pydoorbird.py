efc4151b697ea223accbd64b583275bb0ae78b62
XuanShine <xuan.ng@hotmail.com>
2020-01-30 23:54:24 +0100

    gestion de la reconnection

--- pydoorbird.py
+++ pydoorbird.py
@@ -14,19 +14,25 @@ ip_device = "10.0.0.74"
 # TODO: faire attention au fichier de logs
 logging.basicConfig(filename="doorbird.log", level=logging.DEBUG, format="%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s")
 
-logins_path = os.path.join(os.path.dirname(__file__), '..', "id_doorbird.pkl")
-with open(logins_path, "rb") as f_in:
-    info = pickle.load(f_in)
-user = info["user"]
-password = info["password"]
-del info
+def init():
+    logins_path = os.path.join(os.path.dirname(__file__), '..', "id_doorbird.pkl")
+    with open(logins_path, "rb") as f_in:
+        info = pickle.load(f_in)
+    user = info["user"]
+    password = info["password"]
+    del info
 
-# TODO THREAD
-stream_doorbell = req.get(f"http://{ip_device}/bha-api/monitor.cgi?ring=doorbell", auth=(user, password), stream=True)
-del user
-del password
+    try:
+        stream_doorbell = req.get(f"http://{ip_device}/bha-api/monitor.cgi?ring=doorbell", auth=(user, password), stream=True)
+    except Exception:
+        import traceback
+		logging.error(f"ERROR connection au stream échouée; ip_device: {ip_device}:\n{traceback.format_exc()}")
+        return -1
+    del user
+    del password
+    return stream_doorbell
 
-def watch():
+def watch(stream):
     for elt in stream_doorbell.iter_lines():
         if elt:
             if "doorbell:H" in elt.decode("utf-8"):
@@ -34,7 +40,18 @@ def watch():
                 buzzer.buzz()
 
 def main():
-    watch_entree = Thread(target=watch)
+    try_connection = 0
+    while True:
+        stream = init()
+        if stream != -1:
+            break
+        elif try_connection > 30:
+            raise ConnectionError("Erreur de connexion dans pydoorbird.py")
+        else:
+            try_connection += 1
+            logging.warning(f"Tentative de reconnection dans {try_connection * 10} secondes...")
+            time.sleep(10 * try_connection)
+    watch_entree = Thread(target=watch, args=(stream,))
     watch_entree.start()
 
 if __name__ == "__main__":
